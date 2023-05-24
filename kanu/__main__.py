17cc4b7235704e112d5951a7588456173e9f0f27
Seung-been "Steven" Lee <sbstevenlee@gmail.com>
2023-05-24 17:24:36 +0900

    Update __main__.py

--- kanu/__main__.py
+++ kanu/__main__.py
@@ -5,11 +5,23 @@ import openai
 
 from .version import __version__
 
-def get_input(entry):
-    user_input = entry.get()
-    openai.api_key = user_input
+def _get_key(entry, container, root):
+    openai.api_key = entry.get()
+    container.pack_forget()
+    chat_container = create_chat_container(root)
 
-def send_message(chat_log, entry, messages):
+def create_config_container(root):
+    container = tk.Frame(root)
+    container.pack()
+    label = tk.Label(container, text="OpenAI API Key")
+    label.grid(row=0, column=0, columnspan=2)
+    entry = tk.Entry(container)
+    entry.grid(row=1, column=1)
+    button = tk.Button(container, text="Submit", command=lambda: _get_key(entry, container, root))
+    button.grid(row=1, column=2)
+    return container
+
+def _send_message(chat_log, entry, messages):
     if openai.api_key is None:
         messagebox.showerror("Error", "OpenAI API Key is not set.")
         return
@@ -28,43 +40,31 @@ def send_message(chat_log, entry, messages):
     chat_log.insert(tk.END, "\nBot: " + response)
     entry.delete(0, tk.END)
 
-def clear_chat(chat_log, messages):
-    chat_log.delete(1.0, tk.END)
+def _clear_session(session, messages):
+    session.delete(1.0, tk.END)
     messages.clear()
+
+def create_chat_container(root):
+    container = tk.Frame(root)
+    container.pack()
+    label = tk.Label(container, text="Chat session")
+    label.grid(row=0, column=0, columnspan=2)
+    messages = []
+    session = tk.Text(container, width=70, height=20)
+    session.grid(row=1, column=0, columnspan=2)
+    entry = tk.Entry(container, width=70)
+    entry.grid(row=2, column=0, columnspan=2)
+    send_button = tk.Button(container, text="Send", command=lambda: _send_message(session, entry, messages))
+    send_button.grid(row=3, column=0)
+    clear_butoon = tk.Button(container, text="Clear", command=lambda: _clear_session(session, messages))
+    clear_butoon.grid(row=3, column=1)
 
 def main():
     root = tk.Tk()
     root.title(f"KANU ({__version__})")
     root.geometry("800x700")
 
-    header_font = font.Font(family="Arial", size=20)
-
-    openai_container = tk.Frame(root)
-    openai_container.pack()
-    label = tk.Label(openai_container, text="OpenAI API Key", font=header_font)
-    label.grid(row=0, column=0, columnspan=2)
-
-    openai_api_key_entry = tk.Entry(openai_container)
-    openai_api_key_entry.grid(row=1, column=1)
-
-    openai_api_key_button = tk.Button(openai_container, text="Submit", command=lambda: get_input(openai_api_key_entry))
-    openai_api_key_button.grid(row=1, column=2)
-
-    # This container holds the chat log and the chat entry.
-    chat_container = tk.Frame(root)
-    chat_container.pack()
-    chat_label = tk.Label(chat_container, text="Chat history", font=header_font)
-    chat_label.grid(row=0, column=0, columnspan=2)
-    custom_font = font.Font(family="Arial", size=12)
-    messages = []
-    chat_log = tk.Text(chat_container, width=70, height=20, font=custom_font)
-    chat_log.grid(row=1, column=0, columnspan=2)
-    chat_entry = tk.Entry(chat_container, width=70, font=custom_font)
-    chat_entry.grid(row=2, column=0, columnspan=2)
-    chat_send_button = tk.Button(chat_container, text="Send", command=lambda: send_message(chat_log, chat_entry, messages))
-    chat_send_button.grid(row=3, column=0)
-    chat_clear_butoon = tk.Button(chat_container, text="Clear", command=lambda: clear_chat(chat_log, messages))
-    chat_clear_butoon.grid(row=3, column=1)
+    config_container = create_config_container(root)
 
     root.mainloop()
 
