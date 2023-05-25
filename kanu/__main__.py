f7960a8252173cac73da9a64fd86eec9b45a4011
Seung-been "Steven" Lee <sbstevenlee@gmail.com>
2023-05-25 16:49:59 +0900

    Update __main__.py

--- kanu/__main__.py
+++ kanu/__main__.py
@@ -11,36 +11,49 @@ class KANU:
         self.root.geometry("800x700")
         self.create_home_container()
 
+    def display(self, f, *args, **kwargs):
+        self.container.pack_forget()
+        f(*args, **kwargs)
+
     def create_home_container(self):
         self.container = tk.Frame(self.root)
         self.container.pack()
         label = tk.Label(self.container, text="Welcome to KANU, a Pythonic minimalistic chatbot GUI!")
         label.pack()
-        chatgpt_button = tk.Button(self.container, text="ChatGPT", command=lambda: self.display(self.create_config_container))
+        chatgpt_button = tk.Button(self.container, text="ChatGPT", command=lambda: self.display(self.create_chatgpt_config_container))
         chatgpt_button.pack()
+        docgpt = tk.Button(self.container, text="DocGPT", command=lambda: self.display(self.create_docgpt_config_container))
+        docgpt.pack()
 
     def clear_session(self):
         self.session.delete(1.0, tk.END)
         self.messages.clear()
 
-    def _get_key(self, entry):
-        openai.api_key = entry.get()
-        self.container.pack_forget()
-        self.create_chat_container()
-
-    def display(self, f):
-        self.container.pack_forget()
-        f()
-
-    def create_config_container(self):
+    def create_chatgpt_config_container(self):
         self.container = tk.Frame(self.root)
         self.container.pack()
-        label = tk.Label(self.container, text="OpenAI API Key")
-        label.grid(row=0, column=0, columnspan=2)
+        label = tk.Label(self.container, text="OpenAI API Key:")
+        label.grid(row=0, column=0)
         entry = tk.Entry(self.container)
-        entry.grid(row=1, column=0, columnspan=2)
-        button = tk.Button(self.container, text="Submit", command=lambda: self._get_key(entry))
-        button.grid(row=2, column=0)
+        entry.grid(row=0, column=1)
+        button = tk.Button(self.container, text="Submit", command=lambda: self.display(self.create_chatgpt_session_container, entry.get()))
+        button.grid(row=1, column=0)
+        back_button = tk.Button(self.container, text="Back", command=lambda: self.display(self.create_home_container))
+        back_button.grid(row=1, column=1)
+
+    def create_docgpt_config_container(self):
+        self.container = tk.Frame(self.root)
+        self.container.pack()
+        openai_label = tk.Label(self.container, text="OpenAI API Key:")
+        openai_label.grid(row=0, column=0)
+        huggingface_label = tk.Label(self.container, text="Hugging Face Write Token:")
+        huggingface_label.grid(row=1, column=0)
+        openai_key = tk.Entry(self.container)
+        openai_key.grid(row=0, column=1)
+        huggingface_key = tk.Entry(self.container)
+        huggingface_key.grid(row=1, column=1)
+        submit_button = tk.Button(self.container, text="Submit", command=lambda: self.display(self.create_docgpt_session_container, openai_key.get(), huggingface_key.get()))
+        submit_button.grid(row=2, column=0)
         back_button = tk.Button(self.container, text="Back", command=lambda: self.display(self.create_home_container))
         back_button.grid(row=2, column=1)
 
@@ -52,14 +65,14 @@ class KANU:
             model=self.model.get(),
             messages=self.messages,
         )
-        print(f"used {self.model.get()}")
         response = bot_response["choices"][0]["message"]["content"]
         self.messages += [{"role": "assistant", "content": response}]
         self.session.insert(tk.END, "\nYou: " + entry.get())
         self.session.insert(tk.END, f"\nBot ({self.model.get()}): " + response)
         entry.delete(0, tk.END)
 
-    def create_chat_container(self):
+    def create_chatgpt_session_container(self, openai_key):
+        openai.api_key = openai_key
         self.container = tk.Frame(self.root)
         self.container.pack()
 
@@ -82,9 +95,20 @@ class KANU:
         send_button.grid(row=4, column=0)
         clear_butoon = tk.Button(self.container, text="Clear", command=lambda: self.clear_session())
         clear_butoon.grid(row=4, column=1)
-        back_button = tk.Button(self.container, text="Back", command=lambda: self.display(self.create_config_container))
+        back_button = tk.Button(self.container, text="Back", command=lambda: self.display(self.create_chatgpt_config_container))
         back_button.grid(row=4, column=2)
 
+    def create_docgpt_session_container(self, openai_key, huggingface_key):
+        openai.api_key = openai_key
+        model_id = "sentence-transformers/all-MiniLM-L6-v2"
+        api_url = f"https://api-inference.huggingface.co/pipeline/feature-extraction/{model_id}"
+        headers = {"Authorization": f"Bearer {huggingface_key}"}
+        self.container = tk.Frame(self.root)
+        self.container.pack()
+
+        def query(texts):
+            response = requests.post(api_url, headers=headers, json={"inputs": texts, "options":{"wait_for_model":True}})
+            return response.json()
 
 def main():
     root = tk.Tk()
