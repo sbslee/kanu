170f11cc4a83e126630edc637438070f7b98ef34
Seung-been "Steven" Lee <sbstevenlee@gmail.com>
2023-05-25 10:23:36 +0900

    Enable users to choose their preferred GPT model

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,7 @@
 
 ## 0.2.0 (in development)
 * Enable users to initiate a new chat session.
+* Enable users to choose their preferred GPT model.
 
 ## 0.1.0 (2023-03-24)
 * Initial release.
--- kanu/__main__.py
+++ kanu/__main__.py
@@ -1,80 +1,86 @@
 import tkinter as tk
-from tkinter import messagebox, font
 
 import openai
 
 from .version import __version__
 
-def _get_key(entry, container, root):
-    openai.api_key = entry.get()
-    container.pack_forget()
-    chat_container = create_chat_container(root, container)
+class KANU:
+    def __init__(self, root):
+        self.root = root
+        self.root.title(f"KANU ({__version__})")
+        self.root.geometry("800x700")
+        self.create_config_container()
 
-def create_config_container(root):
-    container = tk.Frame(root)
-    container.pack()
-    label = tk.Label(container, text="OpenAI API Key")
-    label.grid(row=0, column=0, columnspan=2)
-    entry = tk.Entry(container)
-    entry.grid(row=1, column=1)
-    button = tk.Button(container, text="Submit", command=lambda: _get_key(entry, container, root))
-    button.grid(row=1, column=2)
-    return container
+    def go_back(self):
+        self.current.pack_forget()
+        self.previous.pack()
+        self.current = self.previous
 
-def _send_message(chat_log, entry, messages):
-    if openai.api_key is None:
-        messagebox.showerror("Error", "OpenAI API Key is not set.")
-        return
+    def clear_session(self):
+        self.session.delete(1.0, tk.END)
+        self.messages.clear()
 
-    message = entry.get()
-    if not messages:
-        messages.append({"role": "system", "content": "You are a helpful assistant."})
-    messages += [{"role": "user", "content": message}]
-    bot_response = openai.ChatCompletion.create(
-        model='gpt-3.5-turbo',
-        messages=messages,
-    )
-    response = bot_response["choices"][0]["message"]["content"]
-    messages += [{"role": "assistant", "content": response}]
-    chat_log.insert(tk.END, "\nYou: " + message)
-    chat_log.insert(tk.END, "\nBot: " + response)
-    entry.delete(0, tk.END)
+    def _get_key(self, entry):
+        openai.api_key = entry.get()
+        self.current.pack_forget()
+        self.previous = self.current
+        self.create_chat_container()
 
-def _clear_session(session, messages):
-    session.delete(1.0, tk.END)
-    messages.clear()
+    def create_config_container(self):
+        self.current = tk.Frame(self.root)
+        self.current.pack()
+        label = tk.Label(self.current, text="OpenAI API Key")
+        label.grid(row=0, column=0, columnspan=2)
+        entry = tk.Entry(self.current)
+        entry.grid(row=1, column=1)
+        button = tk.Button(self.current, text="Submit", command=lambda: self._get_key(entry))
+        button.grid(row=1, column=2)
 
-def _go_back(current, previous):
-    current.pack_forget()
-    previous.pack()
+    def _send_message(self, entry):
+        if not self.messages:
+            self.messages.append({"role": "system", "content": "You are a helpful assistant."})
+        self.messages += [{"role": "user", "content": entry.get()}]
+        bot_response = openai.ChatCompletion.create(
+            model=self.model.get(),
+            messages=self.messages,
+        )
+        print(f"used {self.model.get()}")
+        response = bot_response["choices"][0]["message"]["content"]
+        self.messages += [{"role": "assistant", "content": response}]
+        self.session.insert(tk.END, "\nYou: " + entry.get())
+        self.session.insert(tk.END, f"\nBot ({self.model.get()}): " + response)
+        entry.delete(0, tk.END)
 
-def create_chat_container(root, previous):
-    container = tk.Frame(root)
-    container.pack()
-    label = tk.Label(container, text="Chat session")
-    label.grid(row=0, column=0, columnspan=3)
-    messages = []
-    session = tk.Text(container, width=70, height=20)
-    session.grid(row=1, column=0, columnspan=3)
-    entry = tk.Entry(container, width=70)
-    entry.grid(row=2, column=0, columnspan=3)
-    send_button = tk.Button(container, text="Send", command=lambda: _send_message(session, entry, messages))
-    send_button.grid(row=3, column=0)
-    clear_butoon = tk.Button(container, text="Clear", command=lambda: _clear_session(session, messages))
-    clear_butoon.grid(row=3, column=1)
-    back_button = tk.Button(container, text="Back", command=lambda: _go_back(container, previous))
-    back_button.grid(row=3, column=2)
-    return container
+    def create_chat_container(self):
+        self.current = tk.Frame(self.root)
+        self.current.pack()
+
+        self.model = tk.StringVar(self.current, value="gpt-3.5-turbo")
+        model_label = tk.Label(self.current, text="Model:")
+        model_label.grid(row=0, column=0)
+        model1_button = tk.Radiobutton(self.current, variable=self.model, text="gpt-3.5-turbo", value="gpt-3.5-turbo")
+        model2_button = tk.Radiobutton(self.current, variable=self.model, text="gpt-4", value="gpt-4")
+        model1_button.grid(row=0, column=1)
+        model2_button.grid(row=0, column=2)
+
+        session_label = tk.Label(self.current, text="Chat session")
+        session_label.grid(row=1, column=0, columnspan=3)
+        self.session = tk.Text(self.current, width=70, height=20)
+        self.session.grid(row=2, column=0, columnspan=3)
+        entry = tk.Entry(self.current, width=70)
+        entry.grid(row=3, column=0, columnspan=3)
+        self.messages = []
+        send_button = tk.Button(self.current, text="Send", command=lambda: self._send_message(entry))
+        send_button.grid(row=4, column=0)
+        clear_butoon = tk.Button(self.current, text="Clear", command=lambda: self.clear_session())
+        clear_butoon.grid(row=4, column=1)
+        back_button = tk.Button(self.current, text="Back", command=lambda: self.go_back())
+        back_button.grid(row=4, column=2)
+
 
 def main():
     root = tk.Tk()
-    root.title(f"KANU ({__version__})")
-    root.geometry("800x700")
-
-    config_container = create_config_container(root)
-    chat_container = create_chat_container(root, config_container)
-    chat_container.pack_forget()
-
+    kanu = KANU(root)
     root.mainloop()
 
 if __name__ == "__main__":
