7cc431cf7772a3683313ffe3bc3588d2a35b0d7b
Seung-been "Steven" Lee <sbstevenlee@gmail.com>
2023-05-25 15:10:15 +0900

    Update __main__.py

--- kanu/__main__.py
+++ kanu/__main__.py
@@ -9,12 +9,15 @@ class KANU:
         self.root = root
         self.root.title(f"KANU ({__version__})")
         self.root.geometry("800x700")
-        self.create_config_container()
+        self.create_home_container()
 
-    def go_back(self):
-        self.current.pack_forget()
-        self.previous.pack()
-        self.current = self.previous
+    def create_home_container(self):
+        self.container = tk.Frame(self.root)
+        self.container.pack()
+        label = tk.Label(self.container, text="Welcome to KANU, a Pythonic minimalistic chatbot GUI!")
+        label.pack()
+        chatgpt_button = tk.Button(self.container, text="ChatGPT", command=lambda: self.display(self.create_config_container))
+        chatgpt_button.pack()
 
     def clear_session(self):
         self.session.delete(1.0, tk.END)
@@ -22,19 +25,24 @@ class KANU:
 
     def _get_key(self, entry):
         openai.api_key = entry.get()
-        self.current.pack_forget()
-        self.previous = self.current
+        self.container.pack_forget()
         self.create_chat_container()
 
+    def display(self, f):
+        self.container.pack_forget()
+        f()
+
     def create_config_container(self):
-        self.current = tk.Frame(self.root)
-        self.current.pack()
-        label = tk.Label(self.current, text="OpenAI API Key")
+        self.container = tk.Frame(self.root)
+        self.container.pack()
+        label = tk.Label(self.container, text="OpenAI API Key")
         label.grid(row=0, column=0, columnspan=2)
-        entry = tk.Entry(self.current)
-        entry.grid(row=1, column=1)
-        button = tk.Button(self.current, text="Submit", command=lambda: self._get_key(entry))
-        button.grid(row=1, column=2)
+        entry = tk.Entry(self.container)
+        entry.grid(row=1, column=0, columnspan=2)
+        button = tk.Button(self.container, text="Submit", command=lambda: self._get_key(entry))
+        button.grid(row=2, column=0)
+        back_button = tk.Button(self.container, text="Back", command=lambda: self.display(self.create_home_container))
+        back_button.grid(row=2, column=1)
 
     def _send_message(self, entry):
         if not self.messages:
@@ -52,29 +60,29 @@ class KANU:
         entry.delete(0, tk.END)
 
     def create_chat_container(self):
-        self.current = tk.Frame(self.root)
-        self.current.pack()
+        self.container = tk.Frame(self.root)
+        self.container.pack()
 
-        self.model = tk.StringVar(self.current, value="gpt-3.5-turbo")
-        model_label = tk.Label(self.current, text="Model:")
+        self.model = tk.StringVar(self.container, value="gpt-3.5-turbo")
+        model_label = tk.Label(self.container, text="Model:")
         model_label.grid(row=0, column=0)
-        model1_button = tk.Radiobutton(self.current, variable=self.model, text="gpt-3.5-turbo", value="gpt-3.5-turbo")
-        model2_button = tk.Radiobutton(self.current, variable=self.model, text="gpt-4", value="gpt-4")
+        model1_button = tk.Radiobutton(self.container, variable=self.model, text="gpt-3.5-turbo", value="gpt-3.5-turbo")
+        model2_button = tk.Radiobutton(self.container, variable=self.model, text="gpt-4", value="gpt-4")
         model1_button.grid(row=0, column=1)
         model2_button.grid(row=0, column=2)
 
-        session_label = tk.Label(self.current, text="Chat session")
+        session_label = tk.Label(self.container, text="Chat session")
         session_label.grid(row=1, column=0, columnspan=3)
-        self.session = tk.Text(self.current, width=70, height=20)
+        self.session = tk.Text(self.container, width=70, height=20)
         self.session.grid(row=2, column=0, columnspan=3)
-        entry = tk.Entry(self.current, width=70)
+        entry = tk.Entry(self.container, width=70)
         entry.grid(row=3, column=0, columnspan=3)
         self.messages = []
-        send_button = tk.Button(self.current, text="Send", command=lambda: self._send_message(entry))
+        send_button = tk.Button(self.container, text="Send", command=lambda: self._send_message(entry))
         send_button.grid(row=4, column=0)
-        clear_butoon = tk.Button(self.current, text="Clear", command=lambda: self.clear_session())
+        clear_butoon = tk.Button(self.container, text="Clear", command=lambda: self.clear_session())
         clear_butoon.grid(row=4, column=1)
-        back_button = tk.Button(self.current, text="Back", command=lambda: self.go_back())
+        back_button = tk.Button(self.container, text="Back", command=lambda: self.display(self.create_config_container))
         back_button.grid(row=4, column=2)
 
 
