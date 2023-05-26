2ed3edfa9e8b6892a2c053b80391ead09b20165c
Seung-been "Steven" Lee <sbstevenlee@gmail.com>
2023-05-26 14:50:54 +0900

    Update files

--- kanu/__main__.py
+++ kanu/__main__.py
@@ -1,45 +1,53 @@
 import tkinter as tk
-
-import openai
+import importlib.util
 
 from .version import __version__
 
 class KANU:
     def __init__(self, root):
+        self.container = None
         self.root = root
         self.root.title(f"KANU ({__version__})")
         self.root.geometry("800x700")
         self.create_home_container()
 
-    def display(self, f, *args, **kwargs):
-        self.container.pack_forget()
-        f(*args, **kwargs)
-
     def create_home_container(self):
+        if self.container is not None:
+            self.container.pack_forget()
         self.container = tk.Frame(self.root)
         self.container.pack()
-        label = tk.Label(self.container, text="Welcome to KANU, a Pythonic minimalistic chatbot GUI!")
+        label = tk.Label(self.container, text="Welcome to KANU, a minimalistic Python-based chatbot GUI")
         label.pack()
-        chatgpt_button = tk.Button(self.container, text="ChatGPT", command=lambda: self.display(self.create_chatgpt_config_container))
+        chatgpt_button = tk.Button(self.container, text="ChatGPT", command=lambda: self.create_chatgpt_config_container())
         chatgpt_button.pack()
         docgpt = tk.Button(self.container, text="DocGPT", command=lambda: self.display(self.create_docgpt_config_container))
         docgpt.pack()
 
-    def clear_session(self):
-        self.session.delete(1.0, tk.END)
-        self.messages.clear()
-
     def create_chatgpt_config_container(self):
+        self.container.pack_forget()
         self.container = tk.Frame(self.root)
         self.container.pack()
-        label = tk.Label(self.container, text="OpenAI API Key:")
-        label.grid(row=0, column=0)
-        entry = tk.Entry(self.container)
-        entry.grid(row=0, column=1)
-        button = tk.Button(self.container, text="Submit", command=lambda: self.display(self.create_chatgpt_session_container, entry.get()))
-        button.grid(row=1, column=0)
-        back_button = tk.Button(self.container, text="Back", command=lambda: self.display(self.create_home_container))
-        back_button.grid(row=1, column=1)
+        title_label = tk.Label(self.container, text="ChatGPT Configuration")
+        title_label.grid(row=0, column=0, columnspan=2)
+        dependency_label = tk.Label(self.container, text="Required Packages:")
+        dependency_label.grid(row=1, column=0, columnspan=2)
+        package1_label = tk.Label(self.container, text="openai")
+        package1_label.grid(row=2, column=0)
+        install1_label = tk.Label(self.container, text="❌" if importlib.util.find_spec("openai") is None else "✅")
+        install1_label.grid(row=2, column=1)
+        key_label = tk.Label(self.container, text="OpenAI API Key:")
+        key_label.grid(row=3, column=0, columnspan=2)
+        key_entry = tk.Entry(self.container)
+        key_entry.grid(row=4, column=0, columnspan=2)
+        submit_button = tk.Button(self.container, text="Submit", command=lambda: self.create_chatgpt_session_container(key_entry.get()))
+        submit_button.grid(row=5, column=0)
+        back_button = tk.Button(self.container, text="Back", command=lambda: self.create_home_container())
+        back_button.grid(row=5, column=1)
+
+    def create_chatgpt_session_container(self, openai_key):
+        from .chatgpt import ChatGPT
+        chatgpt = ChatGPT(self, openai_key)
+        chatgpt.run()
 
     def create_docgpt_config_container(self):
         self.container = tk.Frame(self.root)
@@ -56,47 +64,6 @@ class KANU:
         submit_button.grid(row=2, column=0)
         back_button = tk.Button(self.container, text="Back", command=lambda: self.display(self.create_home_container))
         back_button.grid(row=2, column=1)
-
-    def _send_message(self, entry):
-        if not self.messages:
-            self.messages.append({"role": "system", "content": "You are a helpful assistant."})
-        self.messages += [{"role": "user", "content": entry.get()}]
-        bot_response = openai.ChatCompletion.create(
-            model=self.model.get(),
-            messages=self.messages,
-        )
-        response = bot_response["choices"][0]["message"]["content"]
-        self.messages += [{"role": "assistant", "content": response}]
-        self.session.insert(tk.END, "\nYou: " + entry.get())
-        self.session.insert(tk.END, f"\nBot ({self.model.get()}): " + response)
-        entry.delete(0, tk.END)
-
-    def create_chatgpt_session_container(self, openai_key):
-        openai.api_key = openai_key
-        self.container = tk.Frame(self.root)
-        self.container.pack()
-
-        self.model = tk.StringVar(self.container, value="gpt-3.5-turbo")
-        model_label = tk.Label(self.container, text="Model:")
-        model_label.grid(row=0, column=0)
-        model1_button = tk.Radiobutton(self.container, variable=self.model, text="gpt-3.5-turbo", value="gpt-3.5-turbo")
-        model2_button = tk.Radiobutton(self.container, variable=self.model, text="gpt-4", value="gpt-4")
-        model1_button.grid(row=0, column=1)
-        model2_button.grid(row=0, column=2)
-
-        session_label = tk.Label(self.container, text="Chat session")
-        session_label.grid(row=1, column=0, columnspan=3)
-        self.session = tk.Text(self.container, width=70, height=20)
-        self.session.grid(row=2, column=0, columnspan=3)
-        entry = tk.Entry(self.container, width=70)
-        entry.grid(row=3, column=0, columnspan=3)
-        self.messages = []
-        send_button = tk.Button(self.container, text="Send", command=lambda: self._send_message(entry))
-        send_button.grid(row=4, column=0)
-        clear_butoon = tk.Button(self.container, text="Clear", command=lambda: self.clear_session())
-        clear_butoon.grid(row=4, column=1)
-        back_button = tk.Button(self.container, text="Back", command=lambda: self.display(self.create_chatgpt_config_container))
-        back_button.grid(row=4, column=2)
 
     def create_docgpt_session_container(self, openai_key, huggingface_key):
         openai.api_key = openai_key
---
+++ kanu/chatgpt.py
@@ -0,0 +1,51 @@
+import tkinter as tk
+
+import openai
+
+class ChatGPT:
+    def __init__(self, kanu, openai_key):
+        self.kanu = kanu
+        self.kanu.container.pack_forget()
+        self.kanu.container = tk.Frame(self.kanu.root)
+        self.kanu.container.pack()
+        openai.api_key = openai_key
+
+    def run(self):
+        self.model = tk.StringVar(self.kanu.container, value="gpt-3.5-turbo")
+        model_label = tk.Label(self.kanu.container, text="Model:")
+        model_label.grid(row=0, column=0)
+        model1_button = tk.Radiobutton(self.kanu.container, variable=self.model, text="gpt-3.5-turbo", value="gpt-3.5-turbo")
+        model2_button = tk.Radiobutton(self.kanu.container, variable=self.model, text="gpt-4", value="gpt-4")
+        model1_button.grid(row=0, column=1)
+        model2_button.grid(row=0, column=2)
+        session_label = tk.Label(self.kanu.container, text="Chat session")
+        session_label.grid(row=1, column=0, columnspan=3)
+        self.session = tk.Text(self.kanu.container, width=70, height=20)
+        self.session.grid(row=2, column=0, columnspan=3)
+        entry = tk.Entry(self.kanu.container, width=70)
+        entry.grid(row=3, column=0, columnspan=3)
+        self.messages = []
+        send_button = tk.Button(self.kanu.container, text="Send", command=lambda: self._send_message(entry))
+        send_button.grid(row=4, column=0)
+        clear_butoon = tk.Button(self.kanu.container, text="Clear", command=lambda: self._clear_session())
+        clear_butoon.grid(row=4, column=1)
+        back_button = tk.Button(self.kanu.container, text="Back", command=lambda: self.kanu.create_chatgpt_config_container())
+        back_button.grid(row=4, column=2)
+
+    def _send_message(self, entry):
+        if not self.messages:
+            self.messages.append({"role": "system", "content": "You are a helpful assistant."})
+        self.messages += [{"role": "user", "content": entry.get()}]
+        bot_response = openai.ChatCompletion.create(
+            model=self.model.get(),
+            messages=self.messages,
+        )
+        response = bot_response["choices"][0]["message"]["content"]
+        self.messages += [{"role": "assistant", "content": response}]
+        self.session.insert(tk.END, "\nYou: " + entry.get())
+        self.session.insert(tk.END, f"\nBot ({self.model.get()}): " + response)
+        entry.delete(0, tk.END)
+
+    def _clear_session(self):
+        self.session.delete(1.0, tk.END)
+        self.messages.clear()
