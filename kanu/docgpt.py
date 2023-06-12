a80941ad3a133f44825571a6bb538a9cb84b87eb
Seung-been "Steven" Lee <sbstevenlee@gmail.com>
2023-06-13 08:42:04 +0900

    Add token counter and price monitor in chat window

--- kanu/docgpt.py
+++ kanu/docgpt.py
@@ -17,7 +17,7 @@ from langchain.document_loaders import (
     CSVLoader,
 )
 
-from .utils import Tooltip, Settings
+from .utils import Tooltip, Settings, Tokenizer
 
 DOCUMENT_LOADERS = {
     ".txt": (TextLoader, {"encoding": "utf8"}),
@@ -37,6 +37,7 @@ class DocGPT:
         self.default_chunk_overlap = default_chunk_overlap
         os.environ["OPENAI_API_KEY"] = openai_key
         self.settings = Settings(self)
+        self.tokenizer = Tokenizer(model)
 
     def run(self):
         self.kanu.container.pack_forget()
@@ -106,26 +107,32 @@ class DocGPT:
         self.kanu.container = tk.Frame(self.kanu.root)
         self.kanu.container.pack()
         l = tk.Label(self.kanu.container, text="DocGPT")
-        l.grid(row=0, column=0, columnspan=4)    
+        l.grid(row=0, column=0, columnspan=4)
+        self.tally = tk.Text(self.kanu.container, width=70, height=1)
+        self.tally.insert(tk.END, "Tokens: 0 ($0)\n")
+        self.tally.grid(row=1, column=0, columnspan=4)
         self.session = tk.Text(self.kanu.container, width=70, height=20)
-        self.session.grid(row=1, column=0, columnspan=4)
+        self.session.grid(row=2, column=0, columnspan=4)
         self.session.tag_config("user", **self.settings.get_user_kwargs())
         self.session.tag_config("bot", **self.settings.get_bot_kwargs())
         user_input = tk.Entry(self.kanu.container, width=54)
-        user_input.grid(row=2, column=0, columnspan=4)
+        user_input.grid(row=3, column=0, columnspan=4)
         b = tk.Button(self.kanu.container, text="Send", command=lambda: self.send_message(user_input))
-        b.grid(row=3, column=0)
+        b.grid(row=4, column=0)
         b = tk.Button(self.kanu.container, text="Clear", command=lambda: self.clear_session())
-        b.grid(row=3, column=1)
+        b.grid(row=4, column=1)
         b = tk.Button(self.kanu.container, text="Go back", command=lambda: self.run())
-        b.grid(row=3, column=2)
+        b.grid(row=4, column=2)
         b = tk.Button(self.kanu.container, text="Settings", command=lambda: self.settings.page())
-        b.grid(row=3, column=3)
+        b.grid(row=4, column=3)
 
     def send_message(self, entry):
         self.session.insert(tk.END, "You: " + entry.get() + "\n", "user")
         response = self.qa(entry.get())["answer"]
         self.session.insert(tk.END, "Bot: " + response + "\n", "bot")
+        self.tokenizer.add(entry.get())
+        self.tally.delete(1.0, tk.END)
+        self.tally.insert(tk.END, f"Tokens: {self.tokenizer.total} (${self.tokenizer.cost():.6f})\n")
         entry.delete(0, tk.END)
 
     def go_with_option1(self):
@@ -179,4 +186,5 @@ class DocGPT:
 
     def clear_session(self):
         self.session.delete(1.0, tk.END)
+        self.tally.insert(tk.END, "Tokens: 0 ($0)\n")
 
