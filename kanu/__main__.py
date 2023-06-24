9e73db998996e8405f2d9bdb6e65c09be87a2174
Seung-been Lee <sbstevenlee@gmail.com>
2023-06-24 10:47:45 +0900

    Update file

--- kanu/__main__.py
+++ kanu/__main__.py
@@ -7,7 +7,16 @@ import importlib.util
 from .version import __version__
 from .gui import Tooltip
 
-GPT_MODELS = ["gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-3.5-turbo-0613", "gpt-4", "gpt-4-32k", "gpt-4-0613"]
+GPT_MODELS = [
+    "gpt-3.5-turbo",
+    "gpt-3.5-turbo-0613",
+    "gpt-3.5-turbo-16k",
+    "gpt-3.5-turbo-16k-0613",
+    "gpt-4",
+    "gpt-4-0613",
+    "gpt-4-32k",
+    "gpt-4-32k-0613",
+]
 CHATGPT_PROMPT = """You are a helpful assistant."""
 DOCGPT_PROMPT = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
